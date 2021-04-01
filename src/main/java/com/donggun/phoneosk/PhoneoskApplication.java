9e6e40626546358c1bb2328290b3bebd874abc2e
Lee Donggun <tnpdlzi@naver.com>
2021-04-01 21:46:02 +0900

    init commit

---
+++ src/main/java/com/donggun/phoneosk/PhoneoskApplication.java
@@ -0,0 +1,13 @@
+package com.donggun.phoneosk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneoskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneoskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/donggun/phoneosk/PhoneoskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donggun.phoneosk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneoskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
