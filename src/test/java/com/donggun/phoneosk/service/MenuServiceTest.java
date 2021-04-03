13953256c33fce66077f8e61b25fa4b181b64162
Lee Donggun <tnpdlzi@naver.com>
2021-04-03 17:16:20 +0900

    test: MenuServiceTest 구현

--- src/test/java/com/donggun/phoneosk/service/MenuServiceTest.java
+++ src/test/java/com/donggun/phoneosk/service/MenuServiceTest.java
@@ -1,9 +1,13 @@
 package com.donggun.phoneosk.service;
 
+import com.donggun.phoneosk.domain.Menu;
 import com.donggun.phoneosk.domain.Store;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional
 public class MenuServiceTest {
@@ -13,14 +17,47 @@ public class MenuServiceTest {
     @Autowired StoreService storeService;
 
     @Test
-    public void 메뉴목록() {
+    public void 메뉴추가() {
         //given
-        Store store = new Store();
-        storeService.add(store);
+        Menu menu = Menu.builder()
+                .menuName("test menu")
+                .store(store)
+                .price(10000)
+                .build();
 
         //when
+        Long id = menuService.add(menu);
+        Menu findMenu = menuService.findOne(id);
 
         //then
+        Assertions.assertThat(findMenu).isNotSameAs(menu);
+
+    }
+
+    @Test
+    public void 메뉴목록() {
+        //given
+        Store store = Store.builder()
+                .name("test")
+                .address("address")
+                .close(2200)
+                .open(1000)
+                .phone(1010001000)
+                .build();
+        storeService.add(store);
+
+        Menu menu = Menu.builder()
+                .menuName("test menu")
+                .store(store)
+                .price(10000)
+                .build();
+        menuService.add(menu);
+
+        //when
+        List<Menu> menuList = menuService.findMenus(StoreSearch storeSearch);
+
+        //then
+        Assertions.assertThat(menuList.size()).isEqualTo(1);
 
     }
 }
