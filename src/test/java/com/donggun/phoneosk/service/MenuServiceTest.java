ac5cf8e208822978fca8901b78bc17dbeb2ece65
Lee Donggun <tnpdlzi@naver.com>
2021-04-03 16:32:33 +0900

    test: menu와 store에 대한 service test 추가

---
+++ src/test/java/com/donggun/phoneosk/service/MenuServiceTest.java
@@ -0,0 +1,22 @@
+package com.donggun.phoneosk.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public class MenuServiceTest {
+
+    @Autowired MenuService menuService;
+    @Autowired MenuRepository memberRepository;
+    
+    @Test
+    public void 메뉴목록() {
+        //given
+
+        //when
+
+        //then
+
+    }
+}
---
+++ src/test/java/com/donggun/phoneosk/service/StoreServiceTest.java
@@ -0,0 +1,22 @@
+package com.donggun.phoneosk.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+public class StoreServiceTest {
+
+    @Autowired StoreService storeService;
+    @Autowired StoreRepository memberRepository;
+
+    @Test
+    public void 가게목록() {
+        //given
+
+        //when
+
+        //then
+
+    }
+}
