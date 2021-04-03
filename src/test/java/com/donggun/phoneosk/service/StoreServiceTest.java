7a6b5bf3b60d276d77605b33b809fc0e2b0c0c5e
Lee Donggun <tnpdlzi@naver.com>
2021-04-03 18:23:37 +0900

    test: StoreServiceTest 구현

--- src/test/java/com/donggun/phoneosk/service/StoreServiceTest.java
+++ src/test/java/com/donggun/phoneosk/service/StoreServiceTest.java
@@ -1,22 +1,37 @@
 package com.donggun.phoneosk.service;
 
+import com.donggun.phoneosk.domain.Menu;
+import com.donggun.phoneosk.domain.Store;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Transactional
 public class StoreServiceTest {
 
     @Autowired StoreService storeService;
-    @Autowired StoreRepository memberRepository;
+    @Autowired StoreRepository storeRepository;
 
     @Test
     public void 가게목록() {
         //given
+        Store store = Store.builder()
+                .name("test")
+                .address("address")
+                .close(2200)
+                .open(1000)
+                .phone(1010001000)
+                .build();
+        storeService.add(store);
 
         //when
+        List<Store> storeList = storeService.findStores();
 
         //then
+        Assertions.assertThat(storeList.size()).isEqualTo(1);
 
     }
 }
